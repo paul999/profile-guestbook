65f2cb9b55f9bc84e26a45c9fb31f9cc12d422d1
Paul Sohier <paul999@phpbb.com>
2010-07-25 14:46:03 +0200

    Needed items for tpl

--- root/includes/ucp/ucp_profile_guestbook.php
+++ root/includes/ucp/ucp_profile_guestbook.php
@@ -35,9 +35,20 @@ class ucp_profile_guestbook
 		
 		$user->add_lang('mods/ucp_profile_guestbook');
 		
+		if (!defined('GUESTBOOK_TABLE'))
+		{
+			include("{$phpbb_root_path}includes/constants_guestbook.$phpEx");
+		}		
+		
 		$template->assign_vars(array(
 			'L_TITLE'		=> $user->lang['UCP_PROFILE_GUESTBOOK_SETTINGS'],
 			'S_SELECT_NOTIFY'	=> ($config['profile_guestbook_notification']) ? true : false,
+			'S_NOTIFY_EMAIL'	=> ($user->data['user_gb_notification'] == GB_NOTIFY_EMAIL) ? true : false,
+			'S_NOTIFY_IM'		=> ($user->data['user_gb_notification'] == GB_NOTIFY_IM) ? true : false,
+			'S_NOTIFY_PM'		=> ($user->data['user_gb_notification'] == GB_NOTIFY_PM) ? true : false,
+			'S_NOTIFY_EMAIL_PM'	=> ($user->data['user_gb_notification'] == GB_NOTIFY_EMAIL_PM) ? true : false,
+			'S_NOTIFY_IM_PM'	=> ($user->data['user_gb_notification'] == GB_NOTIFY_IM_PM) ? true : false,
+			'S_NOTIFY_ALL'		=> ($user->data['user_gb_notification'] == GB_NOTIFY_ALL) ? true : false,
 		));
 	}	
 }
