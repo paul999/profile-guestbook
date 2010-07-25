074ab4dfb258d013390c7ed860e122403e582d8a
Paul Sohier <paul999@phpbb.com>
2010-07-25 20:59:29 +0200

    Validation and saving

--- root/includes/ucp/ucp_profile_guestbook.php
+++ root/includes/ucp/ucp_profile_guestbook.php
@@ -38,7 +38,47 @@ class ucp_profile_guestbook
 		if (!defined('GUESTBOOK_TABLE'))
 		{
 			include("{$phpbb_root_path}includes/constants_guestbook.$phpEx");
+		}
+		
+		$submit = (isset($_POST['submit'])) ? true : false;
+		$error = $data = array();
+		$s_hidden_fields = '';
+		
+		add_form_key('ucp_pg');
+		
+		if ($submit)
+		{
+			$data = array(
+				'notifymethod'	=> request_var('notifymethod', (int)$user->data['user_gb_notification']),
+				'notify'	=> request_var('enable_notification', (int)$user->data['user_gb_notification_enabled']),
+			);	
+				
+			if (!check_form_key('ucp_pg'))
+			{
+				$error[] = 'FORM_INVALID';
+			}
+
+			if (!sizeof($error))
+			{
+				$sql_ary = array(
+					'user_gb_notification_enabled'	=> $data['notify'],
+					'user_gb_notification' 		=> $data['notifymethod'],
+				);
+
+				$sql = 'UPDATE ' . USERS_TABLE . '
+					SET ' . $db->sql_build_array('UPDATE', $sql_ary) . '
+					WHERE user_id = ' . $user->data['user_id'];
+				$db->sql_query($sql);
+
+				meta_refresh(3, $this->u_action);
+				$message = $user->lang['PREFERENCES_UPDATED'] . '<br /><br />' . sprintf($user->lang['RETURN_UCP'], '<a href="' . $this->u_action . '">', '</a>');
+				trigger_error($message);
+			}
+
+			// Replace "error" strings with their real, localised form
+			$error = preg_replace('#^([A-Z_]+)$#e', "(!empty(\$user->lang['\\1'])) ? \$user->lang['\\1'] : '\\1'", $error);
 		}		
+				
 		
 		$template->assign_vars(array(
 			'L_TITLE'		=> $user->lang['UCP_PROFILE_GUESTBOOK_SETTINGS'],
@@ -49,6 +89,10 @@ class ucp_profile_guestbook
 			'S_NOTIFY_EMAIL_PM'	=> ($user->data['user_gb_notification'] == GB_NOTIFY_EMAIL_PM) ? true : false,
 			'S_NOTIFY_IM_PM'	=> ($user->data['user_gb_notification'] == GB_NOTIFY_IM_PM) ? true : false,
 			'S_NOTIFY_ALL'		=> ($user->data['user_gb_notification'] == GB_NOTIFY_ALL) ? true : false,
+			
+			'S_ENABLE_NOTIFICATION'	=> ($user->data['user_gb_notification_enabled']) ? true : false,
+			
+			'ERROR'			=> (sizeof($error)) ? implode('<br />', $error) : '',
 		));
 	}	
 }
