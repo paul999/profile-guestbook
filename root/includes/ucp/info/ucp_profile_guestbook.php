91068c7f4f20d27ee49f4a30e88dd65e0576b034
Paul Sohier <paul999@phpbb.com>
2010-07-25 12:23:53 +0200

    First setup for UCP page for user notification

---
+++ root/includes/ucp/info/ucp_profile_guestbook.php
@@ -0,0 +1,38 @@
+<?php
+/**
+*
+* @package ucp
+* @version $Id$
+* @copyright (c) 2010 Paul Sohier
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* @package module_install
+*/
+class ucp_profile_guestbook_info
+{
+	function module()
+	{
+		return array(
+			'filename'	=> 'ucp_profile_guestbook',
+			'title'		=> 'UCP_PROFILE_GUESTBOOK',
+			'version'	=> '1.0.0',
+			'modes'		=> array(
+
+
+			),
+		);
+	}
+
+	function install()
+	{
+	}
+
+	function uninstall()
+	{
+	}
+}
+
+?>
---
+++ root/includes/ucp/ucp_profile_guestbook.php
@@ -0,0 +1,35 @@
+<?php
+/**
+*
+* @package ucp
+* @version $Id$
+* @copyright (c) 2010 Paul Sohier
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+* @package acp
+*/
+class ucp_profile_guestbook
+{
+	var $u_action;
+	var $new_config = array();
+
+	function main($id, $mode)
+	{
+		global $db, $user, $auth, $template;
+		global $config, $phpbb_root_path, $phpbb_admin_path, $phpEx;
+		global $cache;
+	}	
+}
+
+?>
