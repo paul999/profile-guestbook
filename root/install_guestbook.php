a4b8ecb4e287412699e4dc263366308fe68a3bd2
Paul Sohier <paul@phpbb.com>
2011-04-05 12:06:56 +0200

    Fix UMIL version

--- root/install_guestbook.php
+++ root/install_guestbook.php
@@ -258,7 +258,7 @@ $versions = array(
 	'1.0.0-rc2'	=> array(),
 	'1.0.0-rc3'	=> array(),
 	'1.0.0'		=> array(),
-	'1.1.0-dev'	=> array(),
+	'1.1.0-dev1'	=> array(),
 );
 
 // Include the UMIF Auto file and everything else will be handled automatically.
